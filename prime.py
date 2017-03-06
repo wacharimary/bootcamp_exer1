4695eb748f845981d087e4a748b15e7668a89bc9
wacharimary <marywachari@gmail.com>
2017-03-06 20:02:49 +0300

    add prime numbers and test them

---
+++ prime.py
@@ -0,0 +1,22 @@
+elif i==1:
+
+			pass
+
+		elif i%1==0:
+
+			if i%i==0:
+
+				if i%[i in range(0,n)]!=0):
+	 print i
+	       else:
+	       
+	        pass
+	    else:
+	      
+	      pass
+	    
+	    else:
+	  
+	      pass
+    
+        print prime(n)
---
+++ prime_test.py
@@ -0,0 +1,13 @@
+import unitest
+import prime
+class solutionprime(unitest.testcase):
+	def test1(self):
+		self.assertTrue(prime(i,n,%)0):
+	def test2(self):
+		self.assertTrue(prime(i,1,=)0):
+	def test3(self):
+		self.assertTrue(prime(i,n,%)0):
+    def test4(self):
+		self.assertTrue(prime(i,n,%)0):
+	def test1(self):
+		self.assertTrue(prime(i,n,%)0):
